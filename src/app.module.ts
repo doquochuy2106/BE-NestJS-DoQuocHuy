e88e87a8c98819a782201bc262d5b0af2d7d88bb
QuocHuy <quochuy@phutungb2b.com>
2026-06-15 16:48:48 +0700

    18. Sử dụng MongoDB với NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://huydo02062004_db_user:21062004@doquochuy.jjypdmf.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
