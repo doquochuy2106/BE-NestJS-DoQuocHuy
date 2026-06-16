88dba486eb19c176a40c5c293a15baf8c311ed97
QuocHuy <quochuy@phutungb2b.com>
2026-06-16 17:04:06 +0700

    19. ENV Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://huydo02062004_db_user:21062004@doquochuy.jjypdmf.mongodb.net/',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
