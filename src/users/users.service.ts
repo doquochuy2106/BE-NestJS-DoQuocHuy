0359aa521677c5533efa5694f95cb9973ee29c91
QuocHuy <quochuy@phutungb2b.com>
2026-06-20 11:11:30 +0700

    22.2 Hash User’s Password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,15 +4,23 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  getHasPassWord = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+
   async create(email: string, password: string, name: string) {
+    const hasPassWord = this.getHasPassWord(password);
     return await this.userModel.create({
       email,
-      password,
+      password: hasPassWord,
       name,
     });
   }
