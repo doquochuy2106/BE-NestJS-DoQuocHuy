81cb9ed41bad7c72e1311afffa7fa560796a6e38
QuocHuy <quochuy@phutungb2b.com>
2026-07-23 16:03:26 +0700

    52.2 Setup CORS NestJS

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,14 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  //config CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
 
   await app.listen(configService.get<string>('PORT'));
 }
